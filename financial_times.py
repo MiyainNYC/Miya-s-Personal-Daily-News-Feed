47314600dbc2709f3edf0a96e2f27125818c1400
Miya Wang <nwang26@fordham.edu>
2020-07-08 20:55:50 -0400

    Update financial_times.py

--- financial_times.py
+++ financial_times.py
@@ -7,6 +7,8 @@ from email.mime.multipart import MIMEMultipart
 import requests
 from bs4 import BeautifulSoup
 import os
+os.chdir(YOUR_HOME_DIR)
+home_dir = YOUR_HOME_DIR
 
 pd.set_option('display.max_colwidth', -1)
 
@@ -38,11 +40,11 @@ df_fund = pd.DataFrame({'Fund Management':labels_list,
              'Link':urls_list})[['Fund Management','Title','Brief','Link']].set_index('Fund Management')
 
 html = df_fund.to_html()
-with open("/home/jupyter/juzi/ft1.html", "w", encoding="utf-8") as file:
+with open("ft1.html", "w", encoding="utf-8") as file:
     file.writelines('<meta charset="UTF-8">\n')
     file.write(html)
     
-html1  = open('/home/jupyter/juzi/ft1.html').read()
+html1  = open('ft1.html').read()
 
 
 ## capital markets
@@ -79,25 +81,77 @@ df_capital = pd.DataFrame({'Capital Markets':labels,
              'Link':urls})[['Capital Markets','Date','Title','Brief','Link']].set_index('Capital Markets')
 
 html = df_capital.to_html()
-with open("/home/jupyter/juzi/ft2.html", "w", encoding="utf-8") as file:
+with open("ft2.html", "w", encoding="utf-8") as file:
     file.writelines('<meta charset="UTF-8">\n')
     file.write(html)
     
-html2  = open('/home/jupyter/juzi/ft2.html').read()
+html2  = open('ft2.html').read()
+
+
+
+##ipo news
+
+url = "https://seekingalpha.com/market-news/ipos"
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+s = requests.Session()
+r = s.get(url,headers={'User-Agent':user_agent})
+page = r.content
+soup = BeautifulSoup(page,'html.parser')
+today = soup.findAll('li', {"class":"date-title item-date-today"})[0]
+RUN = 0
+list_title = []
+list_desc = []
+list_url = []
+
+while RUN<2:
+
+    #if today['class'] == ['mc', 'marketnews-piano-placeholder']:
+      
+    today = today.nextSibling
+    
+    if today['class'] == ['date-title']:#.findAll('li',{"class":"date-title"}) == []:
+        RUN+=1
+    
+    try:
+        symbol = today.find('a').text
+    except:
+        today = today.nextSibling
+    #print(symbol)
+    list_title.append(symbol)
+    desc = today.text
+    #print(desc)
+    list_desc.append(desc)
+    try:
+        url = [i['href'] for i in today.findAll('a') if 'https://seekingalpha.com/news/' in i['href']][0]
+    except:
+        url = today.findAll('a')[1]['href']
+    list_url.append(url)
+df = pd.DataFrame({'title':list_title,
+              'desc':list_desc,
+              'url':list_url})[['title','desc','url']].set_index('title')
+
+
+html = df.to_html()
+with open("ipo_news.html", "w", encoding="utf-8") as file:
+    file.writelines('<meta charset="UTF-8">\n')
+    file.write(html)
+    
+html3  = open('ipo_news.html').read()
+
 
 
 ## send the email
 
 sender_email = 'mwang@deepmacro.com' 
-receiver_email = "***formiya@gmail.com"
-password = '***********'#input("Type your password and press enter:")
+receiver_email = "wsjformiya@gmail.com"
+password = 'Shirley2027'#input("Type your password and press enter:")
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "FT Capital Markets+Fund Management"
 message["From"] = sender_email
 message["To"] = receiver_email
 
-html = os.linesep.join([html1,html2])
+html = os.linesep.join([html1,html2, html3])
 part2 = MIMEText(html, "html")
 
 # Add HTML/plain-text parts to MIMEMultipart message
@@ -113,5 +167,6 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         sender_email, receiver_email, message.as_string()
     )
     
+
 
 
